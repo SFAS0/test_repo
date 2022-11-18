5d14336083cd3ae735d7e400adb64008d29dd7d7
SFAS0 <sfas7@mail.ru>
2022-11-18 16:22:31 +0300

    comm

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print('asd')
+import sys
+import sqlite3
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.con = sqlite3.connect('coffee.sqlite')
+        cur = self.con.cursor()
+        result = cur.execute(f'select * from cofi').fetchall()
+        if not result:
+            return
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        for i, row in enumerate(result):
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
